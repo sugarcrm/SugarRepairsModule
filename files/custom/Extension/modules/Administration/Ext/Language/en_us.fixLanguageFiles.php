02f5555cb20103df692b817f2f1ebc72bc76b00d
Ken Brill <kbrill@sugarcrm.com>
2016-01-29 14:27:20 -0600

    Initial Commit

---
+++ files/custom/Extension/modules/Administration/Ext/Language/en_us.fixLanguageFiles.php
@@ -0,0 +1,37 @@
+<?php
+/*********************************************************************************
+ * Fix Language Files
+ * Kenneth Brill (kbrill@sugarcrm.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY KENNETH BRILL, KENNETH BRILL DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ *
+ * @category   Custom Language File
+ * @package    fixLanguageFiles
+ * @author     Kenneth Brill <kbrill@sugarcrm.com>
+ * @copyright  2015-2016 SugarCRM
+ * @license    http://www.gnu.org/licenses/agpl.txt
+ * @version    3.0
+ * @link       http://www.sugarforge.org/reviews/?group_id=1300
+ */
+$mod_strings['LBL_FIXLANGUAGEFILES'] = 'Fix Language Files';
+$mod_strings['LBL_FIXLANGUAGEFILES_DESC'] = "Fix known issues in language files prior to upgrade";
---
+++ manifest.php
@@ -0,0 +1,108 @@
+<?PHP
+/*****************************************************************************
+ * The contents of this file are subject to the RECIPROCAL PUBLIC LICENSE
+ * Version 1.1 ("License"); You may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/rpl.php. Software distributed under the
+ * License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+ * either express or implied.
+ *
+ * You may:
+ * a) Use and distribute this code exactly as you received without payment or
+ *    a royalty or other fee.
+ * b) Create extensions for this code, provided that you make the extensions
+ *    publicly available and document your modifications clearly.
+ * c) Charge for a fee for warranty or support or for accepting liability
+ *    obligations for your customers.
+ *
+ * You may NOT:
+ * a) Charge for the use of the original code or extensions, including in
+ *    electronic distribution models, such as ASP (Application Service
+ *    Provider).
+ * b) Charge for the original source code or your extensions other than a
+ *    nominal fee to cover distribution costs where such distribution
+ *    involves PHYSICAL media.
+ * c) Modify or delete any pre-existing copyright notices, change notices,
+ *    or License text in the Licensed Software
+ * d) Assert any patent claims against the Licensor or Contributors, or
+ *    which would in any way restrict the ability of any third party to use the
+ *    Licensed Software.
+ *
+ * You must:
+ * a) Document any modifications you make to this code including the nature of
+ *    the change, the authors of the change, and the date of the change.
+ * b) Make the source code for any extensions you deploy available via an
+ *    Electronic Distribution Mechanism such as FTP or HTTP download.
+ * c) Notify the licensor of the availability of source code to your extensions
+ *    and include instructions on how to acquire the source code and updates.
+ * d) Grant Licensor a world-wide, non-exclusive, royalty-free license to use,
+ *    reproduce, perform, modify, sublicense, and distribute your extensions.
+ *
+ * The Original Code is: SugarCRM
+ *                       Kenneth brill
+ *                       2008-11-03 kbrill@sugarcrm.com
+ *
+ * The Initial Developer of the Original Code is Kenneth Brill
+ * All Rights Reserved.
+ ********************************************************************************/
+$manifest = array(
+
+    'acceptable_sugar_versions' => array (
+        'regex_matches' => array (
+            1 => "6\.5\.*.*",
+            2 => "6\.7\.*.*",
+            3 => "7\.5\.*.*",
+            4 => "7\.6\.*.*",
+            5 => "7\.7\.*.*",
+        ),
+    ),
+    'acceptable_sugar_flavors' => array (
+        0 => 'OS',
+        1 => 'PRO',
+        2 => 'ENT',
+        3 => 'CE',
+        4 => 'ULT',
+    ),
+    'name' 			=> 'SugarCRM: Pre-upgrade repair tool',
+    'description' 		=> 'A tool to repair language files',
+    'author' 		=> 'Ken Brill',
+    'published_date'	=> '12/16/2015',
+    'version' 		=> '3.3',
+    'type' 			=> 'module',
+    'is_uninstallable' 	=> true,
+);
+
+$installdefs = array(
+    'id'=> 'Ken_Brills_Language_File_Fixer',
+    'copy' => array(
+        array('from'=> '<basepath>/files/custom/Extension/application/Ext/EntryPointRegistry/fixLanguageFiles.php',
+            'to'=> 'custom/Extension/application/Ext/EntryPointRegistry/fixLanguageFiles.php',
+        ),
+        array('from'=> '<basepath>/files/custom/Extension/modules/Administration/Ext/Administration/FixLanguageFiles.php',
+            'to'=> 'custom/Extension/modules/Administration/Ext/Administration/FixLanguageFiles.php',
+        ),
+        array('from'=> '<basepath>/files/custom/modules/Administration/fixLanguageFiles.php',
+            'to'=> 'custom/modules/Administration/fixLanguageFiles.php',
+        ),
+        array('from'=> '<basepath>/files/custom/modules/Administration/fixLanguageFiles.css',
+            'to'=> 'custom/modules/Administration/fixLanguageFiles.css',
+        ),
+        array('from'=> '<basepath>/files/custom/modules/Administration/fixLanguageFiles.js',
+            'to'=> 'custom/modules/Administration/fixLanguageFiles.js',
+        ),
+        array('from'=> '<basepath>/files/custom/modules/Administration/fixLanguageFiles.tpl',
+            'to'=> 'custom/modules/Administration/fixLanguageFiles.tpl',
+        ),
+        array('from'=> '<basepath>/files/custom/modules/Administration/fixLanguageFilesResult.tpl',
+            'to'=> 'custom/modules/Administration/fixLanguageFilesResult.tpl',
+        ),
+   ),
+
+    'language'=> array(
+        array('from'=> '<basepath>/files/custom/Extension/modules/Administration/Ext/Language/en_us.fixLanguageFiles.php',
+            'to_module'=> 'Administration',
+            'language'=>'en_us'
+        ),
+    ),
+);
+?>
