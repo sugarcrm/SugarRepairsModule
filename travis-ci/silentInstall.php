06a9d79e7e83e7cfec29698f2b53a0fc9e1562c0
Jerry Clark <geraldbclark@gmail.com>
2016-03-20 17:24:23 -0400

    Update silentInstall.php

--- travis-ci/silentInstall.php
+++ travis-ci/silentInstall.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
-
+//error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+error_reporting(E_ALL);
 $_REQUEST = array(
     'goto' => 'SilentInstall' ,
     'cli'=>'true',
