ee13b6a4bb97c589a616627c0098a34199804d2a
Jerry Clark <geraldbclark@gmail.com>
2016-03-20 17:09:34 -0400

    Update silentInstall.php

--- travis-ci/silentInstall.php
+++ travis-ci/silentInstall.php
@@ -1,8 +1,11 @@
 <?php
 
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+
 $_REQUEST = array(
     'goto' => 'SilentInstall' ,
     'cli'=>'true',
     'instance_url' => 'http://localhost'
 );
+
 require('install.php');
