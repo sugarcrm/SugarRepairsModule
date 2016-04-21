b78206cb9b0b38b107a99cd4ecae06a9351b8ff8
Jerry Clark <geraldbclark@gmail.com>
2016-04-21 09:59:28 -0400

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ $manifest = array(
     'name' => 'Sugar Repairs',
     'published_date' => '2016-01-30 19:27:01',
     'type' => 'module',
-    'version' => '1',
+    'version' => '1.3',
     'remove_tables' => 'prompt',
 );
 
