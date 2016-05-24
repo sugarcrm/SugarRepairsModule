4e0f4e190e70804224c598ee19b2aeffe51d558d
Jerry Clark <geraldbclark@gmail.com>
2016-05-24 14:24:00 -0400

    Update 0.php

--- post_execute/0.php
+++ post_execute/0.php
@@ -1,8 +1,23 @@
 <?php
 
-global $sugar_config;
-
-if ($sugar_config['dbconfig']['db_type'] == 'mysql') {
+//Adjust db columns to store more data
+if ($GLOBALS['sugar_config']['dbconfig']['db_type'] == 'mysql') {
     $GLOBALS['db']->query("ALTER TABLE supp_sugarrepairs MODIFY value_before LONGTEXT");
     $GLOBALS['db']->query("ALTER TABLE supp_sugarrepairs MODIFY value_after LONGTEXT");
 }
+
+//Hack for installation to 6.7
+if (version_compare($GLOBALS['sugar_version'], '6.8', '<') && version_compare($GLOBALS['sugar_version'], '6.7', '>=')) {
+	$files = array(
+		'custom/application/Ext/Include/modules.ext.php',
+		sugar_cached('file_map.php')
+	);
+
+	foreach($files as $file) {
+		if (file_exists($file)) {
+			unlink($file);
+		}
+	}
+}
+
+
