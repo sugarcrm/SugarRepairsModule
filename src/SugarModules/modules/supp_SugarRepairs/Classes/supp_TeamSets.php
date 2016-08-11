5771d2881b446ea6bad2c89db678acc700770f5f
Matt Marum <mmarum@sugarcrm.com>
2016-08-11 14:28:53 -0400

    Fix license headers on application files

--- src/SugarModules/modules/supp_SugarRepairs/Classes/supp_TeamSets.php
+++ src/SugarModules/modules/supp_SugarRepairs/Classes/supp_TeamSets.php
@@ -1,4 +1,5 @@
 <?php
+// Copyright 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
 
 require_once('modules/Teams/TeamSet.php');
 
--- src/post_execute/0.php
+++ src/post_execute/0.php
@@ -1,4 +1,5 @@
 <?php
+// Copyright 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
 
 //Adjust db columns to store more data
 if ($GLOBALS['sugar_config']['dbconfig']['db_type'] == 'mysql') {
